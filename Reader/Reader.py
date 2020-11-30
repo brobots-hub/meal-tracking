fc10d72766745472496b7caca7a1ec98a205b4b4
Andrew Yatsura <andrewyazura203@gmail.com>
2020-11-30 11:24:34 +0200

    Add logging to Reader

--- Reader/Reader.py
+++ Reader/Reader.py
@@ -1,31 +1,29 @@
+import logging
+
 import rdm6300
 from Request import Request
 
 
 class Reader(rdm6300.Reader):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.quit_loop = False
-
     def set_card_callback(self, callback):
         self._callback = callback
 
     def card_inserted(self, card):
         print(f"card inserted {card}")
+        logging.debug(f'card inserted - {card}')
 
     def card_removed(self, card):
         print(f"card removed {card}")
+        logging.debug(f'card removed - {card}')
         r = Request(user_id=str(card.value))
         self._callback(r)
 
     def invalid_card(self, card):
         print(f"invalid card {card}")
+        logging.debug(f'invalid card - {card}')
 
 
 class TestReader:
-    def __init__(self, *args, **kwargs):
-        pass
-
     def set_card_callback(self, callback):
         self._callback = callback
 
